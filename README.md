7f0aaaeb1aed5cc1cafb8f6a2ab405dbc545b6ab
1data <1data@1datadeMacBook-Pro.local>
2018-12-13 11:25:22 +0800

    文档

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Antlr4使用过程:
    按照业务要求定义语法文件.g4
 2、生成语法解析代码 
    2.1使用antlr4生产相关代码
+       java -jar ~/work/antlr-4.7.1-complete.jar -visitor -Dlanguage=Python2 EdataLr.g4
    2.2编写解析逻辑
 3、调用/使用
+   python3.7 edata.py t.txt 
 ```
--- edata.py
+++ edata.py
@@ -15,6 +15,7 @@ from EdataLrSpider import EdataLrSpider
 def main(argv):
     #input = FileStream(argv[1], encoding='utf8')
     input = InputStream('(zhangsan) | (((c:linke) | (t:wangwu)) + (lisi))')
+
     lexer = EdataLrLexer(input)
     stream = CommonTokenStream(lexer)
     parser = EdataLrParser(stream)
